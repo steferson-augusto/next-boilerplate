3c59c9bf54d4c5f7baa4a92059298142b9cb0648
Steferson Augusto <steferson_a@hotmail.com>
2020-10-28 13:12:49 -0400

    :feat: atualizando nextjs para versão 10.0

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+        <h2>Hello World</h2>
+      </main>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export default {
+  colors: {
+    background: '#121212',
+    text: '#E1E1E6',
+    primary: '#BB86FC',
+    secondary: '#03DAC6'
+  }
+}
